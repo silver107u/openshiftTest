2d17a0ed5a6810efabe0d7e07c71dc8e62f43440
root <root@localhost.localdomain>
2020-09-28 09:17:46 +0530

    new commit

--- index.php
+++ index.php
@@ -3,4 +3,7 @@ echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+echo "This ", "string ", "was ", "made ", "with new  parameters.";
+echo "This ", "string ", "was ", "made ", "with super  parameters.";
 ?>
